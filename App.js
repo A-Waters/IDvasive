148ca8f5e3cda17c95628aa132cd9c40f49e149f
A-Waters <alexanderWatermenAWC@Gmail.com>
2021-11-29 17:10:44 -0500

    tfjs react

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
 
 export default function App() {
+
+  tf.ready();
+
   return (
     <View style={styles.container}>
       <Text>it works</Text>
