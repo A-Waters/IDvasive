530d61ffb3645e1baa58f7bcabd4a9735084ddd5
Alex Waters <AlexanderWatermenAWC@gmail.com>
2022-04-23 12:14:18 -0400

    fixed bugs

--- backend/python_server/server.py
+++ backend/python_server/server.py
@@ -30,7 +30,7 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
 
         working = True
-
+        print(self.headers)
 
 
         if "Content-Length" in self.headers:
@@ -38,26 +38,6 @@ class Handler(BaseHTTPRequestHandler):
             body = self.rfile.read(content_length)
             with open(workfile, "wb") as out_file:
                 out_file.write(body)
-            
-        elif "chunked" in self.headers.get("Transfer-Encoding", ""):
-            with open(workfile, "wb") as out_file:
-            
-                while True:
-                    line = self.rfile.readline().strip()
-                    chunk_length = int(line, 16)
-
-                    if chunk_length != 0:
-                        chunk = self.rfile.read(chunk_length)
-                        out_file.write(chunk)
-
-                    # Each chunk is followed by an additional empty newline
-                    # that we have to consume.
-                    self.rfile.readline()
-
-                    # Finally, a chunk size of 0 is an end indication
-                    if chunk_length == 0:
-                        break
-            
             
         else:
             working = False
